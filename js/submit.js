c2b8b905bca4877c1025b6bd0da8e244c35ef548
Otakar Andrysek <otakar@me.com>
2017-01-26 00:18:58 -0800

    Implemented basic MYSQL integration

--- js/submit.js
+++ js/submit.js
@@ -1,12 +1,11 @@
-            (function saveData($) {
-                var person = prompt("Please enter your name", "");
-                $.ajax(
-                {
-                    type: "POST",
-                    url: "http://sstctf.org/typing-test/backend.php",
-                    data: { name: $("select[name='players']").val()},
-                success:function( msg ) {
-                alert( "Data Saved: " + msg );
-                }
-                });
-            })(jQuery); 
+(function saveData($) {
+	var person = prompt('Please enter your name', '');
+	$.ajax({
+		type: 'POST',
+		url: './backend.php2',
+		data: { name: $("select[name='players']").val() },
+		success: function (msg) {
+			alert('Data Saved: ' + msg);
+		}
+	});
+}(jQuery));
---
+++ testscript.php
@@ -0,0 +1,71 @@
+
+<html>
+    
+    <?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed"; 
+    }
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+    
+    <body>
+        <h2>PHP Form Validation Example</h2>
+        <p><span class="error">* required field.</span></p>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+    
+        <?php
+        //$message = "PHP LOADED";
+        //echo "<script type='text/javascript'>alert('$message');</script>";
+        
+        echo "<h2>Your Input:</h2>";
+        echo $name;
+
+$servername = "localhost";
+$username = "www";
+$password = "gfFIw{NHpwCF67";   // TODO: Remove password from index.php by calling password from another file
+$dbname = "typing_test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO test (name)
+VALUES ('$name')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+        
+        
+        ?>
+</html>
